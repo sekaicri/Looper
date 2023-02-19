930120119287ce85a779f6b7a77dbbf2342aece9
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-02-19 07:52:42 -0500

    fgf

--- app/Http/Controllers/objectscene.php
+++ app/Http/Controllers/objectscene.php
@@ -99,7 +99,7 @@ class objectscene extends Controller
   
     public function show(Request $request)
     {
-        $creation = DB::SELECT('(SELECT * FROM objects_scene_events al where events_id = ' . $request->events_id . ')');
+        $creation = DB::SELECT('(SELECT * FROM objects_scene_events al where objects_scenes_id = ' . $request->objects_scenes_id . ')');
         return response()->json([
             'success'   => true,
             'message'   => 'estos son los codigos',
