aa10e91067cc44d7748e5c9aaf2ca213d91c6bc3
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-02-19 11:13:04 -0500

    ghg

--- app/Http/Controllers/objectscene.php
+++ app/Http/Controllers/objectscene.php
@@ -59,7 +59,7 @@ class objectscene extends Controller
         for ($i = 0; $i < $request['num']; $i++) {
             $code = $this->generateUniqueCode();
             $events = objectsSceneEvent::create([
-                'objects_scenes' => $request['objects_scenes_id'],
+                'objects_scenes_id' => $request['objects_scenes_id'],
                 'code' => $code,
             ]);
             $mynames->push($events);
