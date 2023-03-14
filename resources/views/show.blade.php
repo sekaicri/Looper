cb58fa2ab6ba28cef8c5baf119df8f7b08913e81
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-03-14 13:33:00 -0500

    fgfgfgfg

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div>
-        <iframe width="1920" height="1080" src="https://www.youtube.com/embed/{{ $id }}?rel=0&modestbranding=1&autohide=1&mute=1&showinfo=0&controls=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+        <iframe width="1920" height="1080" src="https://www.youtube.com/embed/{{ $id }}?rel=0&modestbranding=1&autohide=1&mute=0&showinfo=0&controls=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
     </div>
 @endsection
