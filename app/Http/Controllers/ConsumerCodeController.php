0a419eeee888e7e0cb4186b63c184f9785534190
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-05-12 08:04:11 -0500

    update

---
+++ app/Http/Controllers/ConsumerCodeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\eventscodes;
+use App\Models\ConsumerCode;
+
+class ConsumerCodeController extends Controller
+{
+    public function store(Request $request)
+{
+    $request->validate([
+        'code' => 'required',
+    ]);
+
+    $code = $request->input('code');
+
+    $eventCode = eventscodes::where('code', $code)->first();
+    if (!$eventCode) {
+        return response()->json([
+            'message' => 'El código de evento no existe.',
+            'exists' => false,
+        ]);
+    }
+
+    $existingConsumerCode = ConsumerCode::where('code', $code)->first();
+    if ($existingConsumerCode) {
+        return response()->json([
+            'message' => 'El código ya esta en uso',
+        ]);
+    }
+
+    // Crear el nuevo registro en ConsumerCode
+    ConsumerCode::create([
+        'code' => $code,
+    ]);
+
+    return response()->json([
+        'message' => 'El registro se ha creado correctamente.',
+        'event_id' => $eventCode->event_id,
+    ]);
+}
+}
