de3ff88ad332b2891e19c882b56dd4de71ca304f
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-06-12 12:11:05 -0500

    update

--- app/Http/Controllers/ConsumerCodeController.php
+++ app/Http/Controllers/ConsumerCodeController.php
@@ -7,63 +7,65 @@ use Illuminate\Http\Request;
 use App\Models\eventscodes;
 use App\Models\ConsumerCode;
 use App\Models\Video;
+use Carbon\Carbon;
 
 use Illuminate\Support\Facades\Validator;
-
 
 class ConsumerCodeController extends Controller
 {
     public function store(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'code' => [
-            'required',
-            'present',
-            function ($attribute, $value, $fail) {
-                if (empty($value)) {
-                    return response()->json([
-                        'message' => 'El código de evento no existe.',
-                    ],400); }
-            },
-        ],
-    ]);
+    {
+        $validator = Validator::make($request->all(), [
+            'code' => [
+                'required',
+                'present',
+                function ($attribute, $value, $fail) {
+                    if (empty($value)) {
+                        return response()->json([
+                            'message' => 'El código de evento no existe.',
+                        ], 400);
+                    }
+                },
+            ],
+        ]);
 
-    if ($validator->fails()) {
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->first('code'),
+            ], 400);
+        }
+
+        $code = $request->input('code');
+
+        $eventCode = eventscodes::where('code', $code)->first();
+
+        if (!$eventCode) {
+            return response()->json([
+                'message' => 'El código de evento no existe.',
+            ], 400);
+        }
+
+        $existingConsumerCode = ConsumerCode::where('code', $code)->first();
+        if ($existingConsumerCode) {
+            return response()->json([
+                'message' => 'El código ya está en uso',
+            ], 400);
+        }
+
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $totalConsumerCodes = ConsumerCode::whereDate('created_at', $currentDate)->count();
+        $counter = floor($totalConsumerCodes / 5);
+
+        // Crear el nuevo registro en ConsumerCode
+        ConsumerCode::create([
+            'code' => $code,
+        ]);
+
+        $eventCode->event_id .= $counter;
+
         return response()->json([
-            'message' => $validator->errors()->first('code'),
-        ], 400);
+            'message' => 'El registro se ha creado correctamente.',
+            'event_id' => $eventCode->event_id,
+        ], 200);
     }
-
-    $code = $request->input('code');
-
-    $eventCode = eventscodes::where('code', $code)->first();
-
-    if (!$eventCode) {
-        return response()->json([
-            'message' => 'El código de evento no existe.',
-        ],400);
-    }
-
-    $existingConsumerCode = ConsumerCode::where('code', $code)->first();
-    if ($existingConsumerCode) {
-        return response()->json([
-            'message' => 'El código ya esta en uso',
-        ],400);
-    }
-    
-    $totalConsumerCodes = ConsumerCode::count();
-    $counter = floor($totalConsumerCodes / 30);
-
-    // Crear el nuevo registro en ConsumerCode
-    ConsumerCode::create([
-        'code' => $code,
-    ]);
-
-    $eventCode->event_id .= $counter;
-
-    return response()->json([
-        'message' => 'El registro se ha creado correctamente.',
-        'event_id' => $eventCode->event_id,
-    ],200);
-}
 }
