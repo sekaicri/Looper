787d6756e52ca8ff2b40f6e659d867e09ddb10d7
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-11-11 10:39:24 -0500

    update

--- database/migrations/2023_11_11_082408_create_battles_table.php
+++ database/migrations/2023_11_11_082408_create_battles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('battleusers', function (Blueprint $table) {
+        Schema::create('battles', function (Blueprint $table) {
             $table->id();
             $table->integer('value');
             $table->boolean('paid')->default(false);
